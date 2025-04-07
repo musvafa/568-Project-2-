40d9dba756915826cf48a08bbc9ac70dcae88b2b
Mustafa Al-Ogaili <mustafaal-ogaili@MacBook-Air-974.local>
2025-04-07 19:35:25 -0400

    Add initial job search tool

---
+++ jobmatch_updated.py
@@ -0,0 +1,70 @@
+import time
+
+from langchain.agents import initialize_agent, Tool
+from langchain.llms import OpenAI
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RAPIDAPI_KEY = os.getenv("LINKEDIN_JOB_SEARCH_RAPIDAPI_KEY") # https://rapidapi.com/fantastic-jobs-fantastic-jobs-default/api/linkedin-job-search-api/playground
+
+def job_search(input: str):
+    # print(f"Tool input: {input}") #for debugging
+    job_title = input.strip()
+    location = "United States"
+
+    url = "https://linkedin-job-search-api.p.rapidapi.com/active-jb-1h"
+    headers = {
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "linkedin-job-search-api.p.rapidapi.com"
+    }
+    params = {
+        "offset": "0",
+        "title_filter": f"\"{job_title}\"",
+        "location_filter": f"\"{location}\""
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        jobs = data.get("title", [])[:3]
+        if not jobs:
+            return "No jobs found."
+        # return "\n".join([f"{job['title']} at {job['company']} ({job['location']})" for job in jobs])
+        return jobs
+    elif response.status_code == 429:
+        time.sleep(2)
+        return "Rate limit hit."
+    else:
+        time.sleep(2)
+        return f"API Error: {response.status_code}"
+
+# Define LangChain tool
+job_search_tool = Tool(
+    name="job_search",
+    func=job_search,
+    description="Use this tool to search for jobs. Input should be a job title or keyword as a string."
+)
+
+llm = OpenAI(base_url="https://openai.vocareum.com/v1", api_key=os.getenv("SI568_OPENAI_API_KEY"), temperature=0.2)
+
+
+agent = initialize_agent(
+    tools=[job_search_tool],
+    llm=llm,
+    agent="zero-shot-react-description",
+    verbose=True
+)
+
+if __name__ == "__main__":
+    job_query = input("Enter your skills or job title: ")
+
+    user_prompt = f"I'm looking for job listings for '{job_query}'. Use the job_search tool."
+
+    response = agent.run(user_prompt)
+    print(response)
+
+jobmatch_1.py
+Displaying jobmatch_1.py.
